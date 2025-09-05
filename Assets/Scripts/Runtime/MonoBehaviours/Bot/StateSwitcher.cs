440af434ea9328f867c09b9274f4af2c24b83a97
FriDriH <good.24165@gmail.com>
2025-09-05 23:13:30 +0300

    AI retreat from bomb (need refactoring of code)
    ИИ умеет убегать от бомбы, после того, как ее поставил. Но код требует рефакторинга: удалить StateSwitcher и все, что связано с ним; более понятно написать BotLogicExecuter, удалить лишние методы и поля.

--- Assets/Scripts/Runtime/MonoBehaviours/Bot/StateSwitcher.cs
+++ Assets/Scripts/Runtime/MonoBehaviours/Bot/StateSwitcher.cs
@@ -1,12 +1,62 @@
 using Interfaces;
+using Runtime.MonoBehaviours.Bot;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.LowLevel;
 using UnityFigmaBridge.Editor.Fonts;
+
+namespace Interfaces 
+{
+    public abstract class StateSwitcher
+    {
+        private Dictionary<string, IState> _states;
+        public Dictionary<string, IState> States => _states;
+        public abstract void LogicUpdate(BotLogicExecuter zombieStateManager);
+        public abstract void SetSpeed(float speed);
+        public abstract void SetTarget(Transform target);
+        public abstract float CheckDistance();
+        public abstract void CheckPath();
+        public abstract void SwitchState(IState newState);
+        public abstract void SetStateChangeAllowed(bool allowed);
+    }
+}
 
 namespace Runtime.MonoBehaviours.Bot
 {
+    public class StateSwitchersDto
+    {
+        private NavMeshAgent _agent;
+        private Transform _player;
+        private Transform _thisBot;
+        private Dictionary<string, IState> _states;
+        private ICharacterRuntimeData _characterData;
+        private BotLogicExecuter _botlogicExecuter;
+
+        public NavMeshAgent Agent => _agent;
+        public Transform Player => _player;
+        public Transform ThisBot => _thisBot;
+        public Dictionary<string, IState> States => _states;
+        public ICharacterRuntimeData CharacterData => _characterData;
+        public BotLogicExecuter BotLogicExecuter => _botlogicExecuter;
+
+        public StateSwitchersDto(
+            NavMeshAgent agent, 
+            Transform player, 
+            Transform thisBot, 
+            Dictionary<string, IState> states, 
+            ICharacterRuntimeData characterData, 
+            BotLogicExecuter botLogicExecuter)
+        {
+            _agent = agent;
+            _player = player;
+            _thisBot = thisBot;
+            _states = states;
+            _characterData = characterData;
+            _botlogicExecuter = botLogicExecuter;
+        }
+    }
     public class StatesSelector
     {
         private Dictionary<BotType, Dictionary<string, IState>> _states;
@@ -19,9 +69,18 @@ namespace Runtime.MonoBehaviours.Bot
                     BotType.Easy,
                     new Dictionary<string, IState>()
                     {
-                        { "Agro", new Agro() },
-                        { "Search Shelter", new SearchShelter() },
-                        { "Deploy Bomb", new DeployBomb() }
+                        { "Agro", new SimpleAgro() },
+                        { "Search Shelter", new SimpleSearchShelter() },
+                        { "Deploy Bomb", new SimpleDeployBomb() }
+                    }
+                },
+                {
+                    BotType.Standart,
+                    new Dictionary<string, IState>()
+                    {
+                        { "Agro", new SimpleAgro() },
+                        { "Search Shelter", new SimpleSearchShelter() },
+                        { "Deploy Bomb", new SimpleDeployBomb() }
                     }
                 }
             };
@@ -40,33 +99,31 @@ namespace Runtime.MonoBehaviours.Bot
         }
     }
 
-    public abstract class StateSwitcher
-    {
-        protected NavMeshAgent _agent;
-        protected Transform _player;
-        protected Transform _target;
-        protected Transform _thisBot;
-        protected Dictionary<string, IState> _states;
-        protected IState _currentState;
-        public IState CurrentState => _currentState;
-        public void Construct(NavMeshAgent agent, Transform player, Transform thisBot)
-        {
-            _agent = agent;
-            _player = player;
-            _thisBot = thisBot;
-        }
-        public abstract void SwitchState(BotAIManager zombieStateManager);
-        public abstract void SetSpeed(float speed);
-        public abstract void SetTarget(Transform target);
-        public abstract float CheckDistance();
-    }
-
+    
     public class EasyStateSwitcher : StateSwitcher
     {
-        public EasyStateSwitcher()
+        private BotLogicExecuter _botlogicExecuter;
+        private NavMeshAgent _agent;
+        private Transform _player;
+        private Transform _target;
+        private Transform _thisBot;
+        private Dictionary<string, IState> _states;
+        private ICharacterRuntimeData _characterData;
+        private IState _currentState;
+        private bool _stateChangeAllowed = true;
+
+        
+        public IState CurrentState => _currentState;
+
+        public EasyStateSwitcher(StateSwitchersDto dto)
         {
-            StatesSelector statesSelector = new StatesSelector();
-            _states = statesSelector.GetStatesForType(BotType.Easy);
+            _thisBot = dto.ThisBot;
+            _agent = dto.Agent;
+            _player = dto.Player;
+            _states = dto.States;
+            _characterData = dto.CharacterData;
+            _botlogicExecuter = dto.BotLogicExecuter;
+            SwitchState(_states["Agro"]);
         }
         public override float CheckDistance()
         {
@@ -94,19 +151,43 @@ namespace Runtime.MonoBehaviours.Bot
             _agent.destination = target.position;
         }
 
-        public override void SwitchState(BotAIManager manager)
-        {            
-            float distance = CheckDistance();
-            IState newState = (distance >= 2) ? _states["Agro"] : _states["Deploy Bomb"];
+        public override void CheckPath()
+        {
+            NavMeshPath path = new NavMeshPath();
+            _agent.CalculatePath(_target.position, path);
+            if (path.status == NavMeshPathStatus.PathComplete)
+            {
+                SetTarget(_target);
+            }
+            else
+            {
+                
+            }
+        }
+        public override void SwitchState(IState newState)
+        {
+            if (!_stateChangeAllowed) return;
+
             if (newState != _currentState)
             {
                 if (_currentState != null)
                 {
-                    _currentState.Exit(manager);
-                }                
+                    _currentState.Exit(_botlogicExecuter);
+                }
                 _currentState = newState;
-                _currentState.Enter(manager);
+                _currentState.Enter(_botlogicExecuter);
             }
+        }
+
+        public override void LogicUpdate(BotLogicExecuter manager)
+        {
+            _currentState.Update(manager);
+            
+        }
+
+        public override void SetStateChangeAllowed(bool allowed)
+        {
+            _stateChangeAllowed = allowed;
         }
     }
 }
