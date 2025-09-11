560d7bbafce36435e73fcb6cc7b951cd51461dd7
FriDriH <good.24165@gmail.com>
2025-09-11 09:47:30 +0300

    Separation of the logic of finding retreat points
    Новый класс, отвечающий за нахождение точек для отступления (RetreatPositionFinder)

--- Assets/Scripts/Runtime/MonoBehaviours/Bot/BotLogicExecuter.cs
+++ Assets/Scripts/Runtime/MonoBehaviours/Bot/BotLogicExecuter.cs
@@ -30,15 +30,13 @@ namespace Runtime.MonoBehaviours.Bot
         private IState _currentState;
         private Vector3 _spawnedBombPos;
         private GameObject _player;
+        private RetreatPositionFinder _retreatPositionFinder;
 
         public Transform TargetOpponent => _targetOpponent;
         public Vector3 Target => _target;
         public Dictionary<string, IState> States => _states;
         public BotCharacter Character => _character;
         public ICharacterRuntimeData CharacterData => _characterData;
-
-        List<Vector3> possiblePos = new List<Vector3>();
-        List<Vector3> availablePos = new List<Vector3>();
 
         private void Awake()
         {
@@ -66,32 +64,11 @@ namespace Runtime.MonoBehaviours.Bot
             _character.PlayerBombDeployer.BombSpawned -= SetBombPosition;
         }
 
-        private void OnDrawGizmos()
-        {            
-            //Gizmos.DrawSphere(_targetOpponent.position, 0.5f);
-            //if (possiblePos != null)
-            //{
-            //    foreach (var point in possiblePos)
-            //    {
-            //        Gizmos.color = UnityEngine.Color.red;
-            //        Gizmos.DrawSphere(point + new Vector3(0, 1, 0), 0.3f);
-            //    }
-            //}
-            //if (availablePos != null)
-            //{
-            //    foreach (var point in availablePos)
-            //    {
-            //        Gizmos.color = UnityEngine.Color.green;
-            //        Gizmos.DrawSphere(point + new Vector3(0, 1, 0), 0.3f);
-            //    }
-            //}
-        }
-
         private void CollectRefs()
         {
             if (TryGetComponent(out BotCharacter character)) _character = character;
             if (TryGetComponent(out NavMeshAgent agent)) _agent = agent;
-
+            _retreatPositionFinder = new RetreatPositionFinder();
             
             _characterData = _character.CharacterRuntimeData;
             
@@ -200,114 +177,13 @@ namespace Runtime.MonoBehaviours.Bot
                 _player = newPlayer;
             }
         }
-
-        private bool PointInBlackList(List<Vector3> blackList, Vector3 point)
-        {
-            Vector3 checkingPoint = new Vector3(point.x, 0, point.z);
-
-            foreach(Vector3 blackListPoint in blackList)
-            {
-                Vector3 blackPoint = new Vector3(blackListPoint.x, 0, blackListPoint.z);
-
-                if (checkingPoint == blackPoint) return true;
-            }
-            return false;
-        }
-
-        private List<Vector3> GenerateBlacklistPositions(byte explosionRange, Vector3 bombPos)
-        {
-            var blacklist = new List<Vector3> { bombPos };
-            for (int i = 1; i <= explosionRange; i++)
-            {
-                blacklist.Add(bombPos + new Vector3(i, 0, 0));
-                blacklist.Add(bombPos + new Vector3(-i, 0, 0));
-                blacklist.Add(bombPos + new Vector3(0, 0, i));
-                blacklist.Add(bombPos + new Vector3(0, 0, -i));
-            }
-            return blacklist;
-        }
-
-        private List<Vector3> GeneratePossiblePositions(byte explosionRange, List<Vector3> blacklist)
-        {
-            var possiblePositions = new List<Vector3>();
-            float centerX = _spawnedBombPos.x;
-            float centerZ = _spawnedBombPos.z;
-
-            for (int x = -explosionRange - 1; x <= explosionRange + 1; x++)
-            {
-                for (int z = -explosionRange - 1; z <= explosionRange + 1; z++)
-                {
-                    Vector3 point = new Vector3(centerX + x, 0, centerZ + z);
-
-                    if (!PointInBlackList(blacklist, point))
-                    {
-                        possiblePositions.Add(point);
-                    }
-                }
-            }
-            if (possiblePositions.Count > 0)
-            {
-                return possiblePositions;
-            }
-            else
-            {
-                Debug.LogError("GeneratePossiblePositions: list of possible positions is null");
-                return null;
-            }
-        }
-        private List<Vector3> FindAvailablePosForRetreat(List<Vector3> possiblePos, List<Vector3> blacklist)
-        {
-            if (possiblePos.Count == 0)
-            {
-                Debug.LogError(gameObject.name + " FindAvailablePosForRetreat: parametr 'possiblePos' is null");
-                return null;
-            }
-            Vector3[] sideOffsets = { new Vector3(1, 0, 0), new Vector3(-1, 0, 0) };
-            var availablePositions = new List<Vector3>();
-
-            foreach (Vector3 point in possiblePos)
-            {
-                if (NavMesh.SamplePosition(point, out NavMeshHit hit, 0.5f, NavMesh.AllAreas))
-                {
-                    NavMeshPath path = new NavMeshPath();
-                    _agent.CalculatePath(hit.position, path);
-
-                    if (path.status == NavMeshPathStatus.PathComplete)
-                    {
-                        availablePositions.Add(hit.position);
-
-                        // проверяем боковую секцию чтобы сразу уйти с поля поражения бомбы
-                        foreach (var offset in sideOffsets)
-                        {
-                            var sidePos = hit.position + offset;
-                            _agent.CalculatePath(sidePos, path);
-                            if (path.status == NavMeshPathStatus.PathComplete && !PointInBlackList(blacklist, sidePos))
-                            {
-                                availablePositions.Add(sidePos);
-                            }
-                        }
-                    }
-                    else continue;
-                }
-            }
-            if (availablePositions.Count > 0)
-            {
-                return availablePositions;
-            }
-            else
-            {
-                Debug.LogError(gameObject.name + " FindAvailablePosForRetreat: did not find available positions for retreat");
-                return null;
-            }
-        }
-
         public void RetreatFromBomb()
         {
             byte explosionRange = (byte)_characterData.BombsSpreading;
 
-            var blacklistPositions = GenerateBlacklistPositions(explosionRange, _spawnedBombPos);
-            var possiblePositions = GeneratePossiblePositions(explosionRange, blacklistPositions);
-            var availablePositions = FindAvailablePosForRetreat(possiblePositions, blacklistPositions);
+            var blacklistPositions = _retreatPositionFinder.GenerateBlacklistPositions(explosionRange, _spawnedBombPos);
+            var possiblePositions = _retreatPositionFinder.GeneratePossiblePositions(explosionRange, blacklistPositions, _spawnedBombPos);
+            var availablePositions = _retreatPositionFinder.FindAvailablePosForRetreat(possiblePositions, blacklistPositions, _agent);
 
             if (availablePositions == null || availablePositions.Count == 0)
             {
@@ -320,9 +196,6 @@ namespace Runtime.MonoBehaviours.Bot
                 int randInt = UnityEngine.Random.Range(0, availablePositions.Count);
                 SetTarget(availablePositions[randInt]);
             }
-
-            availablePos = availablePositions;
-            possiblePos = possiblePositions;
         }
         
         public void SwitchState(IState newState)
